a82b616a46130679e67ee055b6b3d65fda102acc
Werostr <werost@student.agh.edu.pl>
2023-08-04 15:02:52 +0200

    partials in express

--- EJS/index.js
+++ EJS/index.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require("path");
 const redditData = require("./data.json");
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -24,6 +26,10 @@ app.get("/r/:subreddit", (req, res) => {
 app.get("/rand", (req, res) => {
   const num = Math.floor(Math.random() * 10) + 1;
   res.render("random", { rand: num });
+});
+
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
 app.listen(3000, () => {
